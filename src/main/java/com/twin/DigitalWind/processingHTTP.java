3fd1824b0608489494cd9a0fe46bd533133070c6
Artimald <59371589+Artimald@users.noreply.github.com>
2024-06-05 17:18:24 +0500

    add swagger UI

--- src/main/java/com/twin/DigitalWind/processingHTTP.java
+++ src/main/java/com/twin/DigitalWind/processingHTTP.java
@@ -7,23 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 @RestController
 @RequestMapping("/wind")
 public class processingHTTP {
-    @GetMapping("/alldate")
-    public List<String[]> allDate() {
-        System.out.println("GET запрос");
-//        for (String[] record : DigitalWindApplication.records) {
-//            for (String field : record) {
-//                System.out.print(field + " ");
-//            }
-//            System.out.println(); // Переход на новую строку после каждой записи
-//        }
-        return DigitalWindApplication.Noise;
-    }
 
     @PostMapping("/getnoise")
     public String processnoise(@RequestBody String requestIndex) {
